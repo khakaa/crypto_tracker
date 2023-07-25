11108e817e005fc2ab31c927fff632f928c2b82c
khakaa <phr0210@naver.com>
2023-07-25 21:41:46 +0900

    [Format] : 코드 수정 없음

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Coin from "./routes/Coin";
-import Coins from "./routes/Coins";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Coin from './routes/Coin';
+import Coins from './routes/Coins';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/:coinId/*" element={<Coin />} />
-        <Route path="/" element={<Coins />} />
+        <Route path='/:coinId/*' element={<Coin />} />
+        <Route path='/' element={<Coins />} />
       </Routes>
     </BrowserRouter>
   );
